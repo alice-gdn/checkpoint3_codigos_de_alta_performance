d102f4739b56d2655ef85d4c6520caaddc4c3e53
Alice Galdino <alicegaldino741@gmail.com>
2026-04-18 11:51:39 -0300

    criacao do metodo de busca e testes na main

--- src/Tree/ArvoreClientes.java
+++ src/Tree/ArvoreClientes.java
@@ -2,26 +2,53 @@ package Tree;
 
 import Entities.Cliente;
 
+//Arvore que organiza clientes usando como critério o CPF
+
 public class ArvoreClientes {
 
     Node raiz;
+
     public ArvoreClientes() {
         this.raiz = null;
     }
 
-    public void inserir (Cliente valor) {
+    public void inserir(Cliente valor) {
         raiz = inserirRecursivo(raiz, valor);
     }
 
-    public Node inserirRecursivo (Node no, Cliente valor) {
+    private Node inserirRecursivo(Node no, Cliente valor) {
         if (no == null) {
             return new Node(valor);
         }
         if (Integer.parseInt(no.getValor().getCPF()) > Integer.parseInt(valor.getCPF())) {
             no.setEsquerda(inserirRecursivo(no.getEsquerda(), valor));
-        }else if (Integer.parseInt(no.getValor().getCPF()) < Integer.parseInt(valor.getCPF())) {
+        } else if (Integer.parseInt(no.getValor().getCPF()) < Integer.parseInt(valor.getCPF())) {
             no.setDireita(inserirRecursivo(no.getDireita(), valor));
         }
         return no;
+    }
+
+    public Cliente buscar(String cpf) {
+        return buscarRecursivo(raiz, cpf).getValor();
+    }
+
+    private Node buscarRecursivo(Node no, String cpf) {
+        //se chegar em null, o cpf nao existe na arvore
+        if (no == null) {
+            return null;
+        }
+        //caso encontrado retorna o cliente
+        if (Integer.parseInt(no.getValor().getCPF()) == Integer.parseInt(cpf)) {
+            return no;
+        }
+        //caso o cpf seja menor do que o nó atual, busca recursivamente a esquerda
+        if (Integer.parseInt(no.getValor().getCPF()) > Integer.parseInt(cpf)) {
+            return buscarRecursivo(no.getEsquerda(), cpf);
+        }
+        // caso o cpf seja maior que o nó atual, busca recursivamente a direita
+        else {
+            return buscarRecursivo(no.getDireita(), cpf);
+        }
+
     }
 }
