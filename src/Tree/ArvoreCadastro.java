bbb6d8fba8139e816c1720a4bdac19a6a0ba0697
Alice Galdino <alicegaldino741@gmail.com>
2026-04-21 14:43:13 -0300

    criacao do metodo buscar

--- src/Tree/ArvoreCadastro.java
+++ src/Tree/ArvoreCadastro.java
@@ -24,4 +24,29 @@ public class ArvoreCadastro {
         }
         return no;
     }
+
+    public Cliente buscar(String cpf) {
+        return buscarRecursivo(raiz, cpf).getValor();
+    }
+
+    private Node buscarRecursivo(Node no, String cpf) {
+        //se chegar em null, o cpf nao existe na arvore
+        if (no == null) {
+            return null;
+        }
+        //caso encontrado retorna o cliente
+        if (Integer.parseInt(no.getValor().getCPF()) == Integer.parseInt(cpf)) {
+            return no;
+        }
+        //caso o cpf seja menor do que o nó atual, busca recursivamente a esquerda
+        if (Integer.parseInt(no.getValor().getCPF()) > Integer.parseInt(cpf)) {
+            return buscarRecursivo(no.getEsquerda(), cpf);
+        }
+        // caso o cpf seja maior que o nó atual, busca recursivamente a direita
+        else {
+            return buscarRecursivo(no.getDireita(), cpf);
+        }
+
+    }
 }
+
